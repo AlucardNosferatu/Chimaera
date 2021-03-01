5df4c61325e29fb00f8ef5bcab0aa4c0d3379f25
Ebenezer Scrooge <1641367382@qq.com>
2021-03-01 18:57:29 +0800

    Update Chimaera.py

--- Chimaera.py
+++ Chimaera.py
@@ -11,19 +11,28 @@ def swip_rel2kg_rel(swipStr):
 
 
 def kg_node2swip_node(kgStr):
-    return kgStr.lower()
+    return kgStr.replace(' ', '_').lower()
 
 
 def swip_node2kg_node(swipStr):
-    return swipStr.capitalize()
+    return swipStr.capitalize().replace('_', ' ')
 
 
-def kg2swip(results, symbols):
+def kg2swip(results, symbols, node_key=None):
     src_context = []
     for result in results:
-        a = result[symbols[0]]['name']
+        if type(node_key) is dict:
+            a_label = list(result[symbols[0]].labels)[0]
+            a = result[symbols[0]][node_key[a_label]]
+            c_label = list(result[symbols[2]].labels)[0]
+            c = result[symbols[2]][node_key[c_label]]
+        elif type(node_key) is str:
+            a = result[symbols[0]][node_key]
+            c = result[symbols[2]][node_key]
+        else:
+            a = str(result[symbols[0]].identity)
+            c = str(result[symbols[2]].identity)
         b = type(result[symbols[1]]).__name__
-        c = result[symbols[2]]['name']
         src_context.append(
             "{}({}, {}).".format(
                 kg_rel2swip_rel(b),
@@ -36,17 +45,29 @@ def kg2swip(results, symbols):
 
 if __name__ == "__main__":
     kg = init_kg()
-    rel = 'KNOWS'
+    rel = 'WROTE'
     results, symbols = find_rel(kg, rel)
     kg_context = kg2swip(results, symbols)
+    rel2 = 'DIRECTED'
+    results, symbols = find_rel(kg, rel2)
+    kg_context += kg2swip(results, symbols)
+
     rel_swip = kg_rel2swip_rel(rel)
     be_rel_swip = rel_be(rel_swip)
     rel_rel_swip = rel_rel(rel_swip, rel_swip)
-    if rel_in(kg_context, be_rel_swip):
-        kg_context = append_rule(kg_context, 'rel_be', be_rel_swip)
-    elif rel_in(kg_context, rel_swip):
-        kg_context = append_rule(kg_context, 'rel_be', rel_swip)
-        kg_context = append_rule(kg_context, 'rel_rel', [rel_swip, rel_swip])
-    else:
-        raise ValueError('Neither rel not be_rel can be found in context')
-    find_all_match(kg_context, rel_rel_swip)
+    # if rel_in(kg_context, be_rel_swip):
+    #     kg_context = append_rule(kg_context, 'rel_be', be_rel_swip)
+    # elif rel_in(kg_context, rel_swip):
+    #     kg_context = append_rule(kg_context, 'rel_be', rel_swip)
+    #     kg_context = append_rule(kg_context, 'rel_rel', [rel_swip, rel_swip])
+    # else:
+    #     raise ValueError('Neither rel not be_rel can be found in context')
+    kg_context = append_rule(
+        kg_context,
+        'custom',
+        'work_with(X,Y) :- {}(X,Z), {}(Y,Z).'.format(
+            'wrote',
+            'directed'
+        )
+    )
+    find_all_match(kg_context, 'work_with')
