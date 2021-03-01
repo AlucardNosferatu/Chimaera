468f501dcc498a268a943e93748b04426b19e2be
Ebenezer Scrooge <1641367382@qq.com>
2021-03-01 18:21:04 +0800

    rel_rel

--- Chimaera.py
+++ Chimaera.py
@@ -1,5 +1,5 @@
 from KG_CLIENT2 import init_kg, find_rel
-from SWIP_CLIENT2 import find_all_match, rel_be, append_rule, rel_in
+from SWIP_CLIENT2 import find_all_match, rel_be, append_rule, rel_in, rel_rel
 
 
 def kg_rel2swip_rel(kgStr):
@@ -41,10 +41,12 @@ if __name__ == "__main__":
     kg_context = kg2swip(results, symbols)
     rel_swip = kg_rel2swip_rel(rel)
     be_rel_swip = rel_be(rel_swip)
+    rel_rel_swip = rel_rel(rel_swip, rel_swip)
     if rel_in(kg_context, be_rel_swip):
         kg_context = append_rule(kg_context, 'rel_be', be_rel_swip)
     elif rel_in(kg_context, rel_swip):
         kg_context = append_rule(kg_context, 'rel_be', rel_swip)
+        kg_context = append_rule(kg_context, 'rel_rel', [rel_swip, rel_swip])
     else:
         raise ValueError('Neither rel not be_rel can be found in context')
-    find_all_match(kg_context, be_rel_swip)
+    find_all_match(kg_context, rel_rel_swip)
