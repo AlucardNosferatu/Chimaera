4407c6feb70bb8133c488b8ad578a157f4e9cd06
Ebenezer Scrooge <1641367382@qq.com>
2021-03-04 10:06:24 +0800

    Add Function Locator

--- WIN32_HOOK2.py
+++ WIN32_HOOK2.py
@@ -1,8 +1,8 @@
 import ctypes as c
-import win32con as wcon
-import win32api
-import time
 from ctypes import wintypes as w
+
+import win32api
+import win32con as wcon
 import win32process as wproc
 
 
@@ -14,17 +14,9 @@ class MODULEINFO(c.Structure):
     ]
 
 
-pid = 13864  # Minesweeper
+pid = 14692  # Minesweeper
 
 k32 = c.windll.kernel32
-
-# OpenProcess = k32.OpenProcess
-# OpenProcess.argtypes = [w.DWORD, w.BOOL, w.DWORD]
-# OpenProcess.restype = w.HANDLE
-
-ReadProcessMemory = k32.ReadProcessMemory
-ReadProcessMemory.argtypes = [w.HANDLE, w.LPCVOID, w.LPVOID, c.c_size_t, c.POINTER(c.c_size_t)]
-ReadProcessMemory.restype = w.BOOL
 
 GetLastError = k32.GetLastError
 GetLastError.argtypes = None
@@ -35,31 +27,34 @@ CloseHandle.argtypes = [w.HANDLE]
 CloseHandle.restype = w.BOOL
 
 get_module_information_func_name = "GetModuleInformation"
-GetModuleInformation = getattr(c.WinDLL("kernel32"), get_module_information_func_name,
-                               getattr(c.WinDLL("psapi"), get_module_information_func_name))
+GetModuleInformation = getattr(
+    c.WinDLL("kernel32"),
+    get_module_information_func_name,
+    getattr(
+        c.WinDLL("psapi"),
+        get_module_information_func_name
+    )
+)
 GetModuleInformation.argtypes = [w.HANDLE, w.HMODULE, c.POINTER(MODULEINFO), w.DWORD]
 GetModuleInformation.restype = w.BOOL
 
 processHandle = win32api.OpenProcess(wcon.PROCESS_ALL_ACCESS, False, pid)
+
 memory_info = wproc.GetProcessMemoryInfo(processHandle)
-module_info = MODULEINFO()
 module_handles = wproc.EnumProcessModules(processHandle)
+module_info = MODULEINFO()
 for i in range(len(module_handles)):
     module_handle = module_handles[i]
-    res = GetModuleInformation(int(processHandle), module_handle, c.byref(module_info), c.sizeof(module_info))
     module_file_name = wproc.GetModuleFileNameEx(processHandle, module_handle)
+
+    if module_file_name.endswith('KERNEL32.DLL'):
+        mh = win32api.GetModuleHandle(module_file_name)
+        CloseHandle_address = win32api.GetProcAddress(mh, b'CloseHandle')
+        GetLastError_address = win32api.GetProcAddress(mh, b'GetLastError')
+
+    res = GetModuleInformation(int(processHandle), module_handle, c.byref(module_info), c.sizeof(module_info))
     print("ModuleName: {}".format(module_file_name))
     print("EntryPoint: {}".format(module_info.EntryPoint))
     print("SizeOfImage: {}".format(module_info.SizeOfImage))
     print("lpBaseOfDll: {}".format(module_info.lpBaseOfDll))
     print()
-
-addr = 0x00000201870507D0  # Minesweeper.exe module base address
-data = c.c_char()
-bytesRead = c.c_ulonglong()
-while True:
-    result = ReadProcessMemory(int(processHandle), addr, c.byref(data), c.sizeof(data), c.byref(bytesRead))
-    e = GetLastError()
-    print('result: {}, err code: {}, bytesRead: {}'.format(result, e, bytesRead.value))
-    print('data: {}'.format(data.value.decode()))
-    time.sleep(1)
