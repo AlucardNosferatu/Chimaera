a52f334ff823cad94bc40ca14f26b5547bcaa12e
Ebenezer Scrooge <1641367382@qq.com>
2021-03-11 17:05:00 +0800

    Update KG_FSM2.py
    Nice Transitions!!!

---
+++ KG_FSM2.py
@@ -0,0 +1,27 @@
+from transitions import Machine
+
+
+class Matter(object):
+    pass
+
+
+model = Matter()
+
+# The states argument defines the name of states
+states = ['solid', 'liquid', 'gas', 'plasma']
+
+# The trigger argument defines the name of the new triggering method
+transitions = [
+    {'trigger': 'melt', 'source': 'solid', 'dest': 'liquid'},
+    {'trigger': 'evaporate', 'source': 'liquid', 'dest': 'gas'},
+    {'trigger': 'sublimate', 'source': 'solid', 'dest': 'gas'},
+    {'trigger': 'ionize', 'source': 'gas', 'dest': 'plasma'}]
+
+machine = Machine(model=model, states=states, transitions=transitions, initial='solid')
+
+# Test
+print(model.state)  # solid
+model.melt()
+print(model.state)  # liquid
+model.evaporate()
+print(model.state)
